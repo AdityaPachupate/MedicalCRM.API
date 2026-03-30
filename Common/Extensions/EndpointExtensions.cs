3de4eea44d35c97323f39e9c36d2185bce067089
Aditya Pachupate <adityapachupate@gmail.com>
2026-03-30 16:29:46 +0530

    feat: implement WhatsApp notification service, database migration, and endpoint registration infrastructure

---
+++ Common/Extensions/EndpointExtensions.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using CRM.API.Common.Interfaces;
+
+namespace CRM.API.Common.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static void MapEndpoints(this IEndpointRouteBuilder app, Assembly assembly)
+        {
+            var endpoints = assembly.GetTypes()
+                .Where(t => typeof(IEndpoint).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                .Select(Activator.CreateInstance)
+                .Cast<IEndpoint>();
+
+            foreach (var endpoint in endpoints)
+                endpoint.MapEndpoint(app);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using CRM.API.Common.Extensions;
+using CRM.API.Common.Interfaces;
+using CRM.API.Infrastructure.Notifications;
+using CRM.API.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("NeonProductionDb")));
+
+builder.Services.AddScoped<INotificationService, WhatsAppNotificationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +24,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MigrateDatabase();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +32,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapEndpoints(typeof(Program).Assembly);
 
 app.Run();
